0693745acec8247b5b09a5f6ac510c878fa1191c
Kasia Lach <lachkasia4@gmail.com>
2018-10-30 09:52:09 +0100

    RWD touchups, menu + images

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ let hamburger = document.querySelector('.header__hamburger');
 let bars = document.querySelectorAll('.header__hamburger div');
 let logo = document.querySelector('.header__nav-img');
 let body = document.querySelector('body');
+let navElements = document.querySelectorAll('.header__nav li');
+let whitespace = document.querySelector('.empty');
 
 hamburger.addEventListener('click', function (e) {
     bars.forEach(function (element) {
@@ -14,4 +16,8 @@ hamburger.addEventListener('click', function (e) {
     overlay.classList.toggle('active');
     logo.classList.toggle('hidden');
     body.classList.toggle('no-scroll');
+    navElements.forEach(function (element) {
+        element.classList.toggle('hamburger')
+    });
+    whitespace.style.display = whitespace.style.display === 'none' ? '' : 'none';
 });
